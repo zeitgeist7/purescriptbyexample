fba51047fd0c1d9c215a38890702e5892ad5d470
Rajiv Jhoomuck <zeitgeist7@icloud.com>
2018-04-11 21:35:54 +0200

    Minor changes to attempt to correct chapter_2 exercise then start chapter_3

--- chapter_2/my_project/src/Main.purs
+++ chapter_2/my_project/src/Main.purs
@@ -4,12 +4,14 @@ module Main where
 
 import Control.Monad.Eff.Console
 import Math(sqrt, pi)
-import Prelude
+import Prelude((*), (+))
 
+diagonal :: Number -> Number -> Number
 diagonal w h = sqrt(w*w + h*h)
 
 
 -- Solutions to exercises
+circleArea :: Number -> Number
 circleArea r = pi * r * r
 
 main = logShow (diagonal 3.0 4.0)
