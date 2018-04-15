8ad7c567d1ef6067df63de91b1fd2a2575e491fc
Rajiv Jhoomuck <zeitgeist7@icloud.com>
2018-04-15 22:23:50 +0200

    Finish first batch of exercises of this chapter

--- chapter_4/src/Main.purs
+++ chapter_4/src/Main.purs
@@ -4,6 +4,21 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Array (null)
+import Data.Array.Partial (tail)
+import Partial.Unsafe (unsafePartial)
+
+length1 :: forall a. Array a -> Int
+length1 arr =
+  if null arr
+    then 0
+    else 1 + length1 (unsafePartial tail arr)
+
+isEven :: Int -> Boolean
+isEven 0 = true
+isEven 1 = false
+isEven n = isEven (n - 2)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
