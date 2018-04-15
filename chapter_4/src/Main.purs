60a5cabebbc4610e71897c60e76f6c67d4520c18
Rajiv Jhoomuck <zeitgeist7@icloud.com>
2018-04-15 23:51:39 +0200

    Finish exercise on filter, infix operator.

--- chapter_4/src/Main.purs
+++ chapter_4/src/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-import Data.Array (null)
+import Data.Array (null, filter)
 import Data.Array.Partial (tail, head)
 import Partial.Unsafe (unsafePartial)
 
@@ -24,7 +24,7 @@ length1 arr =
 List does not have a map function but can use the <$> infx operator
 For example : (\n -> n * 3) <$> 1..4
 -}
-
+-- Recursion exercises
 isEven :: Int -> Boolean
 isEven 0 = true
 isEven 1 = false
@@ -39,3 +39,20 @@ countEven xs =
     currentContribution :: Int -> Int
     currentContribution x =
       if isEven x then 1 else 0
+
+-- Map, Filter Exercises
+-- 1. calculate squares of an array of a number
+squares :: Array Number -> Array Number
+squares =  map (\n -> n * n)
+-- If there is a typeclass that represents
+-- Int and Number that would be greate!
+
+-- Removenegative numbers
+filterNegatives :: Array Number -> Array Number
+filterNegatives = filter ((<=) 0.0)
+
+-- infix synonym for filter
+infix 8 filter as <$?>
+
+anotherNegativeFilter :: Array Number -> Array Number
+anotherNegativeFilter xs = ((<=) 0.0) <$?> xs
