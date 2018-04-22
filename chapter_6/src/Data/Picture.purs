de8767404314934b6686eaf2fe044effddf40bc5
Rajiv Jhoomuck <zeitgeist7@icloud.com>
2018-04-22 11:03:20 +0200

    Add Picture.purs in project folder and addd `Show` instances for `Shape` and `Point` types.

---
+++ chapter_6/src/Data/Picture.purs
@@ -0,0 +1,118 @@
+module Data.Picture where
+
+import Prelude
+
+import Data.Foldable (foldl)
+import Global as Global
+import Math as Math
+
+data Point = Point
+  { x :: Number
+  , y :: Number
+  }
+
+instance showPoint :: Show Point where
+  show (Point { x, y }) = "(" <> show x <> ", " <> show y <> ")"
+
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+  | Line Point Point
+  | Text Point String
+  | Clipped Picture Shape
+
+type Picture = Array Shape
+
+instance showShape :: Show Shape where
+show (Circle c r) =
+  "Circle [center: " <> show c <> ", radius: " <> show r <> "]"
+show (Rectangle c w h) =
+  "Rectangle [center: " <> show c <> ", width: " <> show w <> ", height: " <> show h <> "]"
+show (Line start end) =
+  "Line [start: " <> show start <> ", end: " <> show end <> "]"
+show (Text loc text) =
+  "Text [location: " <> show loc <> ", text: " <> show text <> "]"
+show (Clipped p s) = "Clipped picture: " <> (p # show <<< (map show)) <> " to " <> show s
+
+-- showPicture :: Picture -> Array String
+-- showPicture = map show
+
+data Bounds = Bounds
+  { top    :: Number
+  , left   :: Number
+  , bottom :: Number
+  , right  :: Number
+  }
+
+showBounds :: Bounds -> String
+showBounds (Bounds b) =
+  "Bounds [top: " <> show b.top <>
+  ", left: "      <> show b.left <>
+  ", bottom: "    <> show b.bottom <>
+  ", right: "     <> show b.right <>
+  "]"
+
+shapeBounds :: Shape -> Bounds
+shapeBounds (Circle (Point { x, y }) r) = Bounds
+  { top:    y - r
+  , left:   x - r
+  , bottom: y + r
+  , right:  x + r
+  }
+shapeBounds (Rectangle (Point { x, y }) w h) = Bounds
+  { top:    y - h / 2.0
+  , left:   x - w / 2.0
+  , bottom: y + h / 2.0
+  , right:  x + w / 2.0
+  }
+shapeBounds (Line (Point p1) (Point p2)) = Bounds
+  { top:    Math.min p1.y p2.y
+  , left:   Math.min p1.x p2.x
+  , bottom: Math.max p1.y p2.y
+  , right:  Math.max p1.x p2.x
+  }
+shapeBounds (Text (Point { x, y }) _) = Bounds
+  { top:    y
+  , left:   x
+  , bottom: y
+  , right:  x
+  }
+shapeBounds (Clipped _ s) = shapeBounds s
+
+union :: Bounds -> Bounds -> Bounds
+union (Bounds b1) (Bounds b2) = Bounds
+  { top:    Math.min b1.top    b2.top
+  , left:   Math.min b1.left   b2.left
+  , bottom: Math.max b1.bottom b2.bottom
+  , right:  Math.max b1.right  b2.right
+  }
+
+intersect :: Bounds -> Bounds -> Bounds
+intersect (Bounds b1) (Bounds b2) = Bounds
+  { top:    Math.max b1.top    b2.top
+  , left:   Math.max b1.left   b2.left
+  , bottom: Math.min b1.bottom b2.bottom
+  , right:  Math.min b1.right  b2.right
+  }
+
+emptyBounds :: Bounds
+emptyBounds = Bounds
+  { top:     Global.infinity
+  , left:    Global.infinity
+  , bottom: -Global.infinity
+  , right:  -Global.infinity
+  }
+
+infiniteBounds :: Bounds
+infiniteBounds = Bounds
+  { top:    -Global.infinity
+  , left:   -Global.infinity
+  , bottom:  Global.infinity
+  , right:   Global.infinity
+  }
+
+bounds :: Picture -> Bounds
+bounds = foldl combine emptyBounds
+  where
+  combine :: Bounds -> Shape -> Bounds
+  combine b shape = union (shapeBounds shape) b
--- chapter_6/src/Main.purs
+++ chapter_6/src/Main.purs
@@ -4,6 +4,52 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Data.Picture
+
+-- main :: forall e. Eff (console :: CONSOLE | e) Unit
+-- main = do
+--   log "Hello sailor!"
+
+
+{-
+`show` is defined in the Prelude module called Show
+The definition might look like this:
+
+class Show a where
+  show :: a -> String
+-}
+
+-- Testing understanding:
+class Define a where
+  define :: a -> Int
+
+data Direction = North | East | South | West
+
+instance defineDirection :: Define Direction where
+  define North = 0
+  define East = 1
+  define South = 2
+  define West = 3
+
+-- Exercise
+-- 1.
+-- Test data
+
+-- Defined a Show instance for Point also
+
+origin :: Point
+origin = Point { x, y }
+  where
+    x = 0.0
+    y = 0.0
+
+c1 :: Shape
+c1 = Circle origin 10.0
+
+picture1 = [
+  Line origin (Point {x: 1.0, y: 2.0}),
+  Text (Point {x: 100.0, y: 100.0}) "Bonzy Paro Gamegame Boule et Ti-Ozoz",
+  Circle (Point {x: -200.0, y: -25.0}) 30.0
+]
+
+clipped = Clipped picture1 (Rectangle origin 25.0 25.0)
